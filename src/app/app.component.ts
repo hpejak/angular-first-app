bd874270b9dd7bc8caf89e697576df9bf957a0d4
hpejak <0PAnT4oMiM@@>
2020-08-06 22:34:47 +0200

    Angular first add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {color:darkred}
+  `]
 })
 export class AppComponent {
   title = 'angular-first-app';
+  name = 'My name';
 }
